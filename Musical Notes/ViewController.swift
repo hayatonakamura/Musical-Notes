908d57e93f8bc178d42e6773293f668698c020be
hayaton <hayaton@bu.edu>
2019-11-23 17:54:04 -0500

    Initial Commit

---
+++ Musical Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Musical Notes
+//
+//  Created by Hayato Nakamura on 2019/11/23.
+//  Copyright © 2019 Hayatopia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Musical NotesTests/Musical_NotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Musical_NotesTests.swift
+//  Musical NotesTests
+//
+//  Created by Hayato Nakamura on 2019/11/23.
+//  Copyright © 2019 Hayatopia. All rights reserved.
+//
+
+import XCTest
+@testable import Musical_Notes
+
+class Musical_NotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
