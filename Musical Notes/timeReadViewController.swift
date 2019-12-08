cbc242d7333891db96c171c697a372a6c6380d1b
hayaton <hayaton@bu.edu>
2019-12-07 21:03:40 -0500

    new progress bar

--- Musical Notes/timeReadViewController.swift
+++ Musical Notes/timeReadViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Charts
 import Foundation
 import AudioToolbox
+import UICircularProgressRing
 
 
 class timeReadViewController: UIViewController {
@@ -33,9 +34,9 @@ class timeReadViewController: UIViewController {
     // MARK: - ReadingTimes
     struct ReadingTimes: Codable {
         let day1: Double
-        let day2, day3: Int
+        let day2, day3: Double
         let day4: Double
-        let day5, day6: Int
+        let day5, day6: Double
         let day7: Double
 
         enum CodingKeys: String, CodingKey {
@@ -51,18 +52,26 @@ class timeReadViewController: UIViewController {
 
 
     @IBOutlet weak var lineChart: LineChartView!
+    @IBOutlet weak var goalChart: HorizontalBarChartView!
+    @IBOutlet weak var progressRing: UICircularProgressRing!
+    @IBOutlet weak var welcome_text: UILabel!
     
-    var lineChartEntry: [ChartDataEntry] = []
+    // Change any of the properties you'd like
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        update_text()
+        update_goal()
         update_weekly_time()
+        
+        self.progressRing.minValue = 0
+        self.progressRing.maxValue = 100
         
         // Do any additional setup after loading the view.
     }
     
-    func update_weekly_time(){
+    func update_text(){
         let u = "http://3.231.6.81:5000/time_by_days"
         guard let url = URL(string: u) else {return}
                let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
@@ -70,20 +79,100 @@ class timeReadViewController: UIViewController {
                    //print(String(data: data, encoding: .utf8)!)
                    let book = try? JSONDecoder().decode(Readingtimes.self, from: data)
                 
-                let day1:Double = book!.readingTimes.day1
-                let day2:Double = Double(book!.readingTimes.day2)
-                let day3:Double = Double(book!.readingTimes.day3)
-                let day4:Double = book!.readingTimes.day4
-                let day5:Double = Double(book!.readingTimes.day5)
-                let day6:Double = Double(book!.readingTimes.day6)
-                let day7:Double = Double(book!.readingTimes.day7)
-                
-                
-                    print(day1)
-                
+                let total_hours = (book!.totalTimeInHours)
+                let new_total_hours = String(format: "%.2f", total_hours)
+                                
                    DispatchQueue.main.async {
-                    let x = [day1, day2, day3, day4, day5, day6, day7] as! [Double]
-                    let d = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+                    self.welcome_text.text = "You have read " + new_total_hours + " this week"
+                   }
+                }
+               task.resume()
+    }
+    
+    func test_ring(){
+        self.progressRing.startProgress(to: 49, duration: 2.0) {
+          print("Done animating!")
+          // Do anything your heart desires...
+        }
+    }
+    
+    func update_goal() {
+        
+        self.goalChart.drawBarShadowEnabled = true
+        self.goalChart.drawValueAboveBarEnabled = true
+        self.goalChart.maxVisibleCount = 100
+        self.goalChart.rightAxis.axisMaximum = 100
+        self.goalChart.leftAxis.axisMaximum = 100
+                
+        let xAxis  = self.goalChart.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = false
+        xAxis.granularity = 10.0
+        
+        let leftAxis = self.goalChart.leftAxis;
+        leftAxis.drawAxisLineEnabled = false;
+        leftAxis.drawGridLinesEnabled = true;
+        leftAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+        leftAxis.enabled = false
+        
+        let rightAxis = self.goalChart.rightAxis
+        rightAxis.enabled = true;
+        rightAxis.drawAxisLineEnabled = false;
+        rightAxis.drawGridLinesEnabled = true;
+        rightAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+        
+        let l = self.goalChart.legend
+        l.enabled =  false
+        goalChart.fitBars = true;
+        self.goalChart.animate(xAxisDuration: 2, yAxisDuration: 2)
+        setDataCount()
+    }
+    
+    
+    func setDataCount(){
+        
+        let barWidth = 0.6
+        var yVals = [BarChartDataEntry]()
+        yVals.append(BarChartDataEntry(x: Double(1.0), y: 50))
+//        for i in 0..<count{
+//            let mult = (range + 1)
+//            let val = (Double)(arc4random_uniform(UInt32(mult)))
+//            yVals.append(BarChartDataEntry(x: Double(i) * spaceForBar, y: val))
+//        }
+        
+//        self.goalChart.xAxis.valueFormatter = IndexAxisValueFormatter(values:["Current", "Goal"])
+//        self.goalChart.xAxis.granularity = 1
+        
+        var set1 : BarChartDataSet!
+        set1 = BarChartDataSet(entries: yVals, label: "DataSet")
+        var dataSets = [BarChartDataSet]()
+        dataSets.append(set1)
+        let data = BarChartData(dataSets: dataSets)
+        data.barWidth =  barWidth;
+        goalChart.data = data
+    
+    }
+    
+    func update_weekly_time() {
+        let u = "http://3.231.6.81:5000/time_by_days"
+        guard let url = URL(string: u) else {return}
+               let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                   guard let data = data else { return }
+                   //print(String(data: data, encoding: .utf8)!)
+                   let book = try? JSONDecoder().decode(Readingtimes.self, from: data)
+                
+                let day1 = Double(book!.readingTimes.day1)
+                let day2 = Double(book!.readingTimes.day2)
+                let day3 = Double(book!.readingTimes.day3)
+                let day4 = Double(book!.readingTimes.day4)
+                let day5 = Double(book!.readingTimes.day5)
+                let day6 = Double(book!.readingTimes.day6)
+                let day7 = Double(book!.readingTimes.day7)
+                
+                                
+                   DispatchQueue.main.async {
+                    let x = [day1, day2, day3, day4, day5, day6, day7]
                     print(x)
                     self.lineChart.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
                     self.lineChart.translatesAutoresizingMaskIntoConstraints = false
@@ -108,6 +197,8 @@ class timeReadViewController: UIViewController {
                     guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else {print ("gradient error"); return }
                     lineChartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
                     lineChartDataSet.drawFilledEnabled = true
+                    
+                    self.test_ring()
                    }
                 }
                task.resume()
@@ -116,7 +207,12 @@ class timeReadViewController: UIViewController {
     @IBAction func refresh_pressed(_ sender: Any) {
         print("refresh pressed")
         update_weekly_time()
-    AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        DispatchQueue.main.async {
+            self.progressRing.resetProgress()
+        }
+        test_ring()
+        update_goal()
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
     }
     
     /*
